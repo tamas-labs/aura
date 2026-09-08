bc37e9ea224762237ed8dac0e83ffdbe31a4f15e
Tamas Balint <tamas.balint@digi.hu>
2026-09-08 14:37:03 +0200

    new build

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-import{A as r,k as e,E as s,k as a,m as o,q as u,w as t,x as i}from"./index-CEd1Bs13.js";export{r as Aura,e as AuraPlugin,s as ErrorHandler,a as default,o as useApiResourcesStore,u as useConfigStore,t as useCoreStore,i as useErrorHandlerStore};
+import{A as r,k as e,E as s,k as a,m as o,q as u,w as t,x as i}from"./index-F3oNIlla.js";export{r as Aura,e as AuraPlugin,s as ErrorHandler,a as default,o as useApiResourcesStore,u as useConfigStore,t as useCoreStore,i as useErrorHandlerStore};
